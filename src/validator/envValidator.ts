ad9d34741e87bbe35679e986f0328a1fb38cb8ea
Bibek-only <bibekbibek966@gmail.com>
2026-06-29 22:53:33 +0530

    db implementation doen

--- src/validator/envValidator.ts
+++ src/validator/envValidator.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["DEV", "TEST", "PROD"]).default("DEV"),
   PORT: z.coerce.number().int().min(1).max(65535).default(3000),
+  DATABASE_URL: z.string("Database url must needed")
 });
 
 const validEnv = envSchema.parse(process.env);
